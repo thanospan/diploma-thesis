8eafb3868cb6cf6ed32371be06368e9615060965
Thanos Panagiotidis <thanospan@users.noreply.github.com>
2021-04-06 11:13:23 +0300

    Add policy status check

--- utils/ameaDbUtil.js
+++ utils/ameaDbUtil.js
@@ -6,24 +6,16 @@ const { Amea } = require('../models/amea');
 const { Club } = require('../models/club');
 const ameaMasking = require('./ameaMasking');
 const clubMasking = require('./clubMasking');
+const { policyStatus } = require('../models/policy');
 
 exports.getAll = async (policies) => {
-  let ameaPolicy;
-  let clubPolicy;
-  let amea;
-
-  for (const policy of policies) {
-    if (policy.resource === "amea") {
-      ameaPolicy = policy;
-    }
-    if (policy.resource === "club") {
-      clubPolicy = policy;
-    }
-  }
+  const ameaPolicy = policies.find(policy => ((policy.resource === "amea") && (policy.status === policyStatus.ACTIVE))); 
+  const clubPolicy = policies.find(policy => ((policy.resource === "club") && (policy.status === policyStatus.ACTIVE))); 
 
   const ameaPipeline = ameaMasking.buildPipeline(ameaPolicy);
   const clubPipeline = clubMasking.buildPipeline(clubPolicy);
 
+  let amea;
   /*
   ameaPipeline and clubPipeline are [] only if the role has policies for the amea and clubs resources
   with both excluded and masked array fields set to []. That means that the user can access the resources without any constraint.
