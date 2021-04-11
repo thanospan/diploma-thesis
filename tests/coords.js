c9199aed4e9508fc45405e6d2c0ce478b99e2ec5
Thanos Panagiotidis <thanospan@users.noreply.github.com>
2021-04-11 23:41:32 +0300

    Add loader to coords test

--- tests/coords.js
+++ tests/coords.js
@@ -1,25 +1,36 @@
 'use strict';
 
-const fs = require('fs');
+const fs = require('fs').promises;
 
 const coordsUtil = require('../utils/coords');
+const { Loader } = require('../utils/loader');
 
 const center = { lat: 38.234809, lng: 21.748981 };
 const radius = 100; // meters
 let coords = [];
 
-try {
-  for (let i = 0; i < 5000; i++) {
-    coords.push(coordsUtil.getCoordsWithinRadius(center, radius));
-    // console.log(coords[i].lat + ', ' + coords[i].lng);
-  }
+const loader = new Loader();
 
-  fs.writeFile('coords.csv', 'latitude, longitude\n', () => {});
-  for (const coordsEl of coords) {
-    fs.appendFile('coords.csv', coordsEl.lat + ', ' + coordsEl.lng + '\n', () => {});
-  }
+(async () => {
+  try {
+    loader.start("---Saving coordinates to coords.csv");
 
-  console.log("---Coordinates saved to coords.csv");
-} catch (err) {
-  console.log(err);
-}
+    for (let i = 0; i < 5000; i++) {
+      coords.push(coordsUtil.getCoordsWithinRadius(center, radius));
+      // console.log(coords[i].lat + ', ' + coords[i].lng);
+    }
+
+    await fs.writeFile('coords.csv', 'latitude, longitude\n', () => {});
+    for (const coordsEl of coords) {
+      await fs.appendFile('coords.csv', coordsEl.lat + ', ' + coordsEl.lng + '\n', () => {});
+    }
+
+    loader.stop()
+    console.log('\n---Done');
+    process.exit();
+  } catch (err) {
+    loader.stop()
+    console.log(err);
+    process.exit();
+  }
+})();
